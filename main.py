d7372758db00464640a37eb9743e796850bca974
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-12-09 19:53:42 -0500

    restructured, have an account log in final format

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-# import pandas as pd
-import pandas as pd
-import numpy as np
-import collections as cts
+"""
+Program that processes the set CSV transaction file to create an account log with balances based on the transactions
+"""
+import utilities
+import sys
 
-# Get the CSV file as dataframe
-csv_main = pd.read_csv('./resources/transactions.csv', low_memory=False)
 
-# Get the accounts by customer id from dataframe, make it a dict, will be of lists
-csv_mod1 = pd.read_csv('./resources/transactions.csv', low_memory=False, header=0, usecols=['customerId', 'accountId'], index_col=None)
-csv_mod1_dict = csv_mod1.to_dict(orient='split')
-cust_acct_mod = csv_mod1_dict['data']
+def main():
 
-# Create a dictionary of customer ids mapped to accounts
-id_dict = {}
-for item in cust_acct_mod:
-    key, value_to_add = item
-    if key in id_dict and value_to_add not in id_dict[key]:
-        id_dict[key].append(value_to_add)
+    # Get the list of transactions to process
+    transaction_list = utilities.get_txns_list()
 
-# Output data needs to be a json array
-# Each thing in the array is an object for the customer/customer id
-# The customer object  with comma separated json objects within for each customer
-# within each customer object, list of objects -> account number, balance
+    # Get the accounts by customer id from dataframe, make it a dict, will be of lists
+    cust_accts_list = utilities.get_cust_acct_list()
+
+    # Create a dictionary of customer ids mapped to accounts
+    cust_acct_log = utilities.get_account_log(cust_accts_list)
+
+    # Process transactions to update the account log -> aka update balance of each account in log for each transaction
+    updated_account_log = utilities.process_transactions(cust_acct_log, transaction_list)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
 
 # import json -> json package / standard library for handling json
 
@@ -40,6 +40,3 @@ for item in cust_acct_mod:
 # example - if dict has lists and dicts inside, the type conversion for json will be correct
 #
 # json dumps does reverse?  put a dictionary into json?
-
-
-
--- utilities.py
+++ utilities.py
@@ -1,14 +1,65 @@
-import csv
+import pandas as pd
 
 
-# Function to empty rows 1 to n from a csv file into a list
-def get_csv_data(filepath):
+# Function to get a list of transactions from a csv file
+def get_txns_list() -> list:
 
-    with open(f'{filepath}', 'r') as csvfile:
-        # create the object of csv.reader()
-        csv_file_reader = csv.reader(csvfile,delimiter=',')
-        for row in csv_file_reader:
-            print(row)
+    csv_txn_df = pd.read_csv('./resources/transactions.csv', low_memory=False, header=0,
+                         usecols=['customerId', 'accountId', 'transactionType', 'amount'], index_col=None)
+    csv_txn_dict = csv_txn_df.to_dict(orient='split')
+    txn_list = csv_txn_dict['data']
+    return txn_list
 
 
+# Function to get a list of customers and accounts from given CSV
+def get_cust_acct_list() -> list:
+    csv_df = pd.read_csv('./resources/transactions.csv', low_memory=False, header=0,
+                           usecols=['customerId', 'accountId'], index_col=None)
+    csv_dict = csv_df.to_dict(orient='split')
+    cust_acct_list = csv_dict['data']
+    return cust_acct_list
 
+
+# Function to get account log (list of dictionaries, each dictionary has 2 kvps, cust id and account,
+# Accounts is a list of dictionaries, each dictionary is has 2 kvps account number and balance
+def get_account_log(cust_acct_list: list) -> list:
+
+    cust_acct_dict = make_cust_acct_dict(cust_acct_list)
+    cust_acct_log = list(cust_acct_dict.values())
+
+    # Loop through the dictionaries in the cust acct list, update the lists in them
+    for ca_dict in cust_acct_log:
+
+        # Get the list of accounts for dict, lcs to change each account to dict with account number and balance
+        accts = ca_dict['accounts']
+        updated_accts_list = [{'accountNumber': account, 'balance': 0} for account in accts]
+        ca_dict['accounts'] = updated_accts_list
+
+    return cust_acct_log
+
+
+# Helper function to create a dictionary where each cust id is the key to a dictionary of cust id and accounts
+def make_cust_acct_dict(a_list: list) -> dict:
+
+    cust_acct_dict = {}
+    for item in a_list:
+        cust_id, account_val = item
+        if cust_id not in cust_acct_dict:
+            cust_acct_dict[cust_id] = {'id': cust_id, 'accounts': []}
+        elif account_val not in cust_acct_dict[cust_id]['accounts']:
+            cust_acct_dict[cust_id]['accounts'].append(account_val)
+    
+    return cust_acct_dict
+
+
+# Function to process transactions on the accounts' balances in the account log
+def process_transactions(account_log: list, txns: list):
+
+    # Now to figue out how to process transactions...
+    # could make a transaction list like i made a list to make an account log
+    # then i'd need some looping to go trough the transaction list 1 at a time
+    # grab the cust id from transaction list, find matching cust id in account log
+    # grab accounts, for accounts
+    # find where acct id from txn list match acct num from accounts
+    # look at transaction type, update that balance in the log
+    pass
